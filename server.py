0199094637cba632aae50d0b4bacc62e8a765890
Johnny Rivera <Johnnyr.97@csu.fullerton.edu>
2026-04-06 18:58:12 -0700

    Added database implementation.

--- server.py
+++ server.py
@@ -1,3 +1,18 @@
 import socket       #Networking
 import threading    #Multiple Clients
 import sqlite3      #User Database
+
+#Initialize a user database with SQLite3.
+def init_db():
+    con = sqlite3.connect('chat.db')        #Creates (opens if already created) a user database file. 
+    cur = con.cursor()                      #Creates a cursor to run SQL commands.
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (  
+            username TEXT PRIMARY KEY, 
+            password TEXT NOT NULL
+        )
+    ''')                                    #Creates table, username column (no two usernames can be
+    )                                       #the same), password column (cannot be empty).
+    con.commit()                            #Saves changes to the .db file
+    con.close()                             #Closes connection to database
+    
