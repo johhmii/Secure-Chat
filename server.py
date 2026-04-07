841ccbdd66dfca926b0c0bab9ad891eb1744b6c2
Johnny Rivera <Johnnyr.97@csu.fullerton.edu>
2026-04-06 19:08:46 -0700

    Added register user function.

--- server.py
+++ server.py
@@ -13,6 +13,17 @@ def init_db():
         )
     ''')                                    #Creates table, username column (no two usernames can be
     )                                       #the same), password column (cannot be empty).
-    con.commit()                            #Saves changes to the .db file
-    con.close()                             #Closes connection to database
-    
+    con.commit()                            #Saves changes to the .db file.
+    con.close()                             #Closes connection to database.
+
+#Register user function implementation.
+def register_user(username, password):
+    con = sqlite3.connect('chat.db')        #Opens .db file.
+    cur = con.cursor()                      #Creates a cursor to run SQL commands.
+    try:
+        cur.execute("INSERT INTO users (username, password) VALUES (?,?)", (username, password))    #Insert new user to database.
+        con.commit()                        #Save changes to database.
+        print("User", username, "registerred successfully!")
+    except sqlite3.IntegrityError:          #Executes if username exists.
+        print("Username", username, "already exists!")
+    con.close                               #Close the .db connection.
